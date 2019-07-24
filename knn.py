0a275a8f424da96b390b4f4bc07715f38ef1ee72
trungbanh <trungbanh97@gmail.com>
2019-07-24 15:31:40 +0700

    knn

--- knn.py
+++ knn.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 import os
 import pickle
+import pandas
 
 CLASSIFY = {
     'fruit': 1,
@@ -29,7 +30,6 @@ def read(path='./knowledge/') -> np:
         for path in d:
             for r, _, f in os.walk('./knowledge/'+path):
                 print(r)
-                # print(f)
                 for img in f:
                     img = cv2.imread(r+'/'+img)
                     img = cv2.resize(img, (100, 100))
@@ -37,6 +37,27 @@ def read(path='./knowledge/') -> np:
                     labels.append(CLASSIFY[r.split('/')[2]])
 
     return np.array(images), labels
+
+
+def hogDescriptor():
+    winSize = (20, 20)
+    blockSize = (10, 10)
+    blockStride = (5, 5)
+    cellSize = (10, 10)
+    nbins = 9
+    derivAperture = 1
+    winSigma = -1.
+    histogramNormType = 0
+    L2HysThreshold = 0.2
+    gammaCorrection = 1
+    nlevels = 64
+    useSignedGradients = True
+
+    hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize,
+                            nbins, derivAperture, winSigma, histogramNormType,
+                            L2HysThreshold, gammaCorrection, nlevels,
+                            useSignedGradients)
+    return hog
 
 
 def getFeature():
@@ -48,6 +69,11 @@ def getFeature():
         :return images, labels
         :rtype numpy, list
     """
+
+    descriptions = []
+
+    hog = hogDescriptor()
+
     with open('images.pkl', 'rb') as file:
         unpickler = pickle.Unpickler(file)
         images = unpickler.load()
@@ -56,15 +82,52 @@ def getFeature():
         unpickler = pickle.Unpickler(file)
         labels = unpickler.load()
 
-    return images, labels
+    for image in images:
+        descript = hog.compute(image)
+        xi = [float(x[0]) for x in descript]
+        descriptions.append(xi)
+
+    return descriptions, labels
 
 
-# images, labels = read()
+def SVM_Classifycaition(X, y):
+    svm = cv2.ml.SVM_create()
+    # Set SVM type
+    svm.setType(cv2.ml.SVM_C_SVC)
+    # Set SVM Kernel to Radial Basis Function (RBF)
+    svm.setKernel(cv2.ml.SVM_RBF)
+    # Set parameter C
+    svm.setC(1)
+    # Set parameter Gamma
+    svm.setGamma(1)
+    # Train SVM on training data
+    svm.train(np.float32(X), cv2.ml.ROW_SAMPLE, np.int32(y))
+    # Save trained model
+    # svm.save("hand.yml")
+    print(type(np.float32(X)))
+    print(np.float32(X).shape)
 
-# with open('images.pkl', 'wb') as file:
-#     pickle.dump(images, file)
 
-# with open('labels.pkl', 'wb') as file:
-#     pickle.dump(labels, file)
+def predict(image):
 
-# getFeature()
+    hog = hogDescriptor()
+
+    svm = cv2.ml.SVM_load('hand.yml')
+    descript = hog.compute(image)
+    xi = [float(x[0]) for x in descript]
+
+    xi = np.array(xi)
+    xi = [xi]
+
+    testResponse = svm.predict(np.float32(xi))[1]
+    print(testResponse)
+
+    print(type(np.float32(xi)))
+    print(np.float32(xi).shape)
+
+
+# image = cv2.imread('mau_thu.png')
+
+# image = cv2.resize(image, (100, 100))
+
+# predict(image)
--- readframe.py
+++ readframe.py
@@ -1,23 +1,24 @@
 import cv2
 
+
 def read(path, video):
-    '''
+    """
         @get  path, video as string
 
         @return void
-    '''
+    """
 
     realpath = path + video
     i = 0
     myvideo = cv2.VideoCapture(realpath)
     while(myvideo.isOpened()):
         ret, frame = myvideo.read()
-        i+=1
-        cv2.imshow('frame',frame)
+        i += 1
+        cv2.imshow('frame', frame)
         if (cv2.waitKey(0) == ord('c')):
-            cv2.imwrite('data/'+str(i)+'.png',frame)
+            cv2.imwrite('data/'+str(i)+'.png', frame)
 
-        if (cv2.waitKey(0) == ord('q')) :
+        if (cv2.waitKey(0) == ord('q')):
             break
 
     myvideo.release()
@@ -26,4 +27,4 @@ def read(path, video):
 path = './mua/khmer/'
 video = 'Khmer-Chol-Chnam-Thmay.webm'
 
-read (path, video)
+read(path, video)
